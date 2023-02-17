d22059106b4608c6d2015e66f787d646f91deef2
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-17 13:14:06 +0000

    Create PopularProfiles.js

---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import appStyles from '../../App.module.css'
+
+const PopularProfiles = () => {
+  return (
+    <Container className={appStyles.Content}>
+        <p>Most followed profiles.</p>
+    </Container>
+  )
+}
+
+export default PopularProfiles
