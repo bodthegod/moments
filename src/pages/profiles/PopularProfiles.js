139b09a6821446876912a79937b1e2adcc673e81
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-18 10:57:34 +0000

    Update PopularProfiles.js

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import appStyles from '../../App.module.css'
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import appStyles from "../../App.module.css";
 
 const PopularProfiles = () => {
+  const [profileData, setProfileData] = useState({
+    // we will use the pageProfile later!
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  const {popularProfiles} = profileData;
+
+  useEffect(() => {
+    const handleMount = async => {
+        try {
+            
+        } catch (err) {
+            
+        }
+    }
+  
+    return () => {
+      second
+    }
+  }, [third])
+  
   return (
     <Container className={appStyles.Content}>
-        <p>Most followed profiles.</p>
+      <p>Most followed profiles.</p>
     </Container>
-  )
-}
+  );
+};
 
-export default PopularProfiles
+export default PopularProfiles;
