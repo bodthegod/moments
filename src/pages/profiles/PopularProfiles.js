1e592e71d793a07b9cf2cd7c76d71af73e0b1ba7
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-18 23:44:18 +0000

    create profile ordering by follower count, add loading spinner

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const PopularProfiles = () => {
   const [profileData, setProfileData] = useState({
@@ -8,25 +11,39 @@ const PopularProfiles = () => {
     pageProfile: { results: [] },
     popularProfiles: { results: [] },
   });
-  const {popularProfiles} = profileData;
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
 
   useEffect(() => {
-    const handleMount = async => {
-        try {
-            
-        } catch (err) {
-            
-        }
-    }
-  
-    return () => {
-      second
-    }
-  }, [third])
-  
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
   return (
     <Container className={appStyles.Content}>
-      <p>Most followed profiles.</p>
+      {popularProfiles.results.length ? (
+        <>
+          <p>Most followed profiles.</p>
+          {popularProfiles.results.map((profile) => (
+            <p key={profile.id}>{profile.owner}</p>
+          ))}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
     </Container>
   );
 };
