f600800f506b7c236318fc7265becaf76c53793a
joeplaydondev <77025894+bodthegod@users.noreply.github.com>
2023-03-05 18:16:15 +0000

    Update Asset.js

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
 import styles from "../styles/Asset.module.css";
-
+// spinner animation asset code
 const Asset = ({ spinner, src, message }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
